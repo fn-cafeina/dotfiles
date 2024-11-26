1bf2b03aa72db8a07758b0c5e196f004f741014e
fn-cafeina <jasmirp5@gmail.com>
2024-11-25 20:30:59 -0600

    add scripts

---
+++ scripts/shadows-toggle.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+xfconf-query -c xfwm4 -p /general/show_dock_shadow -s false
+xfconf-query -c xfwm4 -p /general/show_popup_shadow -s false
+
+sleep 1
+
+xfconf-query -c xfwm4 -p /general/show_dock_shadow -s true
+xfconf-query -c xfwm4 -p /general/show_popup_shadow -s true
