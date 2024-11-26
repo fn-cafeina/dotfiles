6e84c967c3346ada4a9ae343fd774439f9714b64
fn-cafeina <jasmirp5@gmail.com>
2024-11-25 22:21:21 -0600

    update shadows-toggle script

--- scripts/shadows-toggle.sh
+++ scripts/shadows-toggle.sh
@@ -3,7 +3,5 @@
 xfconf-query -c xfwm4 -p /general/show_dock_shadow -s false
 xfconf-query -c xfwm4 -p /general/show_popup_shadow -s false
 
-sleep 1
-
 xfconf-query -c xfwm4 -p /general/show_dock_shadow -s true
 xfconf-query -c xfwm4 -p /general/show_popup_shadow -s true
