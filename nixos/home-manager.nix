302e3bc5e1ecf05da08e7197652cdaccb83a4c0f
fn-cafeina <jasmirp5@gmail.com>
2024-12-08 00:59:34 -0600

    add special args to home manager

--- nixos/home-manager.nix
+++ nixos/home-manager.nix
@@ -9,7 +9,7 @@
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
-    extraSpecialArgs = { inherit inputs };
+    extraSpecialArgs = { inherit inputs; };
 
     users.cafeina = import ../home;
   };
